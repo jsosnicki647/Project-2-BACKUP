e10b750de6edd34de29b6fda1a74a075ad38c6d7
Robert Pittman <robertamoah271@gmail.com>
2019-10-08 18:51:14 -0400

    added the route for the nodemailer

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,3 +25,56 @@ module.exports = function(app) {
     res.send("404");
   });
 };
+
+
+
+
+
+
+// nodemailer
+
+app.post("/form", (req, res, next) => {
+
+  var email = req.body.email;
+  
+  var name = req.body.name;
+  
+  console.log(email, name)
+  
+  const output =`<p> You have a new contact request </p>
+      <P> 😎Thank you for joing the squad 😎</p>
+      <ul>     
+      <li>Name: ${req.body.name}</li>
+      <li>email: ${req.body.email}</il>
+      </ul>`;
+  
+  const nodemailer = require('nodemailer');
+  
+  let transporter =  nodemailer.createTransport({
+      service : "gmail",
+  
+      auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+      }
+  });
+  
+  
+  
+  let mailoptions  = {
+      from: process.env.EMAIL, // sender address
+      to: `${email}`, // list of receivers
+      subject: 'registration ✔', // Subject line
+      text: `Hey ${name}`, // plain text body
+      html: output
+  };
+  
+  transporter.sendMail(mailoptions, (err, data) =>{
+  if (err){
+      console.log(err)
+  }else{ console.log("email sent")}
+  });
+  
+  
+  // res.send("its okay")
+  })
